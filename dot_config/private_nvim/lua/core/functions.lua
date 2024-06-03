71affbde2315e36e598d3a22cb568ecb9bd9e132
Nacho114 <17376073+Nacho114@users.noreply.github.com>
2024-06-03 15:42:58 +0200

    updated nvim

--- dot_config/private_nvim/lua/core/functions.lua
+++ dot_config/private_nvim/lua/core/functions.lua
@@ -1,10 +1,12 @@
--- Function to insert custom text with the current date
+vim.api.nvim_set_keymap("n", "<leader>ta", "<Esc>:SuperCA<CR>a", {noremap = true, silent = true})
+
 local function insert_custom_text()
   local current_date = os.date("%d %B %Y")
-  local text_to_insert = "- [ ]  | " .. current_date
+  local current_filename = vim.fn.expand("%:t:r")
+  local text_to_insert = "- [ ] " .. vim.fn.getline('.') .. " | " .. current_date .. " | #" .. current_filename
 
-  -- Insert text at the current cursor position
-  vim.api.nvim_put({text_to_insert}, "c", false, true)
+  -- Replace the current line with the new text
+  vim.api.nvim_set_current_line(text_to_insert)
 
   -- Move the cursor to the position after the closing bracket
   vim.cmd("normal! F]l")
@@ -14,4 +16,4 @@ end
 vim.api.nvim_create_user_command("SuperCA", insert_custom_text, {})
 
 -- Create a keymap to trigger the command
-vim.api.nvim_set_keymap("n", "<leader>ca", "<Esc>:SuperCA<CR>a", {noremap = true, silent = true})
+vim.api.nvim_set_keymap("n", "<leader>ta", ":SuperCA<CR>", {noremap = true, silent = true})
