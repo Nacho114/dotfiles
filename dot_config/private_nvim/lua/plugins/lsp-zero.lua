dc531d08f65357bceaa25180e17ff39c8d40ab1a
Nacho114 <17376073+Nacho114@users.noreply.github.com>
2024-12-22 19:37:28 +0100

    added marksman to default lsp

--- dot_config/private_nvim/lua/plugins/lsp-zero.lua
+++ dot_config/private_nvim/lua/plugins/lsp-zero.lua
@@ -88,7 +88,7 @@ return {
 
             require('mason-lspconfig').setup({
                 -- run :help lspconfig-all to see the list of all lsp servers
-                ensure_installed = { 'rust_analyzer', 'pyright', 'svelte', 'yamlls', 'jsonls', 'html', 'cssls', 'lua_ls' },
+                ensure_installed = { 'rust_analyzer', 'pyright', 'svelte', 'yamlls', 'jsonls', 'html', 'cssls', 'lua_ls', 'marksman' },
                 handlers = {
                     lsp_zero.default_setup,
                     lua_ls = function()
