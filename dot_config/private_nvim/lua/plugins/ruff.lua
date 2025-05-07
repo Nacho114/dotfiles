2e83f12169ce283617808c07c6a72b5f2afa43c8
Nacho114 <17376073+Nacho114@users.noreply.github.com>
2025-05-07 08:59:54 +0200

    wip

--- dot_config/private_nvim/lua/plugins/ruff.lua
+++ dot_config/private_nvim/lua/plugins/ruff.lua
@@ -11,7 +11,10 @@ return {
             -- Set up autocommand for running linter
             vim.api.nvim_create_autocmd({ "BufWritePost" }, {
                 callback = function()
-                    lint.try_lint()
+                    -- Only run if ruff is available
+                    if vim.fn.executable('ruff') == 1 then
+                        lint.try_lint()
+                    end
                 end,
             })
         end
@@ -29,6 +32,29 @@ return {
                 format_on_save = {
                     timeout_ms = 500,
                     lsp_fallback = true,
+                    -- Custom formatter that checks if ruff exists first
+                    async = function(bufnr)
+                        if vim.fn.executable('ruff') == 1 then
+                            return conform.format({ bufnr = bufnr, timeout_ms = 500, lsp_fallback = true })
+                        end
+                        return false
+                    end,
+                },
+            })
+        end
+    },
+    {
+        'nvimtools/none-ls.nvim',
+        dependencies = {
+            'nvim-lua/plenary.nvim',
+        },
+        event = { "BufReadPre", "BufNewFile" },
+        config = function()
+            local null_ls = require("null-ls")
+            
+            null_ls.setup({
+                sources = {
+                    null_ls.builtins.code_actions.ruff,
                 },
             })
         end
